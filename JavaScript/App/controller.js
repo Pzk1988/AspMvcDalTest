fd6fd49e494dbb32fac6e0179daa2203e1d67777
pzk1988 <knapik.przemyslaw@gmail.com>
2017-05-28 16:10:05 +0200

    finish material css

---
+++ JavaScript/App/controller.js
@@ -0,0 +1,12 @@
+﻿app.controller('listController', function ($scope) {
+    $scope.products = ['mleko', 'jablko'];
+
+    $scope.addItem = function () {
+        $scope.products.push($scope.newProduct);
+        $scope.newProduct = "";
+    };
+
+    $scope.removeItem = function (i) {
+        $scope.products.splice(i, 1);
+    };
+});
