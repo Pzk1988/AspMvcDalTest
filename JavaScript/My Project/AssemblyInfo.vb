1cd3a6a9320c581316cc25c66f89a6f650903ad2
pzk1988 <knapik.przemyslaw@gmail.com>
2017-05-28 14:37:52 +0200

    Added js project

---
+++ JavaScript/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("JavaScript")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Bombardier Transportation (ZWUS) Polska Sp.z o.o.")>
+<Assembly: AssemblyProduct("JavaScript")>
+<Assembly: AssemblyCopyright("Copyright © Bombardier Transportation (ZWUS) Polska Sp.z o.o. 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1e1db42a-f26a-4ff1-b5e6-79b2b0ce5f2a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
